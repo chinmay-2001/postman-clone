eff7f222c7d46562045390a8684f08b7de625f0d
Chinmay <chinmaygumgaonkar2@gmail.com>
2022-10-10 14:29:03 +0530

    checking

--- Backend/backend.js
+++ Backend/backend.js
@@ -5,31 +5,31 @@ const cors=require("cors")
 const app=express()
 app.use(cors())
 
-const redis=require('redis')
-const redisClient =Redis.createClinet()
-const DEFAULT_EXPIRATION=3600
+// const redis=require('redis')
+// const redisClient =Redis.createClinet()
+// const DEFAULT_EXPIRATION=3600
 
 app.get("/photo",async (req,res)=>{
     
-    redisClient.get('photo',async (error,photo)=>{
-        if(error) console.error(error)
-        if(photo!=null){
-            return res.json(JSON.parse(photo));
-        }
-        else{
-            const {data}=await axios.get(
-                "https://fakestoreapi.com/products",
-            )
-            console.log(data)
-            redis.setex("photo",DEFAULT_EXPIRATION,JSON.stringify(data))
-            res.json(data)
-        }
-    })
-        // const {data}=await axios.get(
-        //     "https://fakestoreapi.com/products",
-        // )
-        // console.log(data)
-        // res.json(data)
+    // redisClient.get('photo',async (error,photo)=>{
+    //     if(error) console.error(error)
+    //     if(photo!=null){
+    //         return res.json(JSON.parse(photo));
+    //     }
+    //     else{
+    //         const {data}=await axios.get(
+    //             "https://fakestoreapi.com/products",
+    //         )
+    //         console.log(data)
+    //         redis.setex("photo",DEFAULT_EXPIRATION,JSON.stringify(data))
+    //         res.json(data)
+    //     }
+    // })
+        const {data}=await axios.get(
+            "https://fakestoreapi.com/products",
+        )
+        console.log(data)
+        res.json(data)
 
     
 })
