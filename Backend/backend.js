8c22dd649475af14955347ce39170d6862e01db5
Chinmay <chinmaygumgaonkar2@gmail.com>
2022-10-11 20:25:36 +0530

    redis cache implemented Changes in Backend.js

--- Backend/backend.js
+++ Backend/backend.js
@@ -3,42 +3,59 @@ const axios =require("axios")
 const cors=require("cors")
 
 const app=express()
+app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
 const redis=require('redis')
 // const redisClient =redis.createClient()
-
-let func=async () => {
+let redisClient;
+(async () => {
     redisClient=redis.createClient();
     redisClient.on("error",(error)=>console.error(`Error:${error}`));
     await redisClient.connect();
-};
-func();
+    console.log("inside func")
+})();
+const DEFAULT_EXPIRATION=3600
+// func();
 app.get("/photo",async (req,res)=>{
-    
-    redisClient.get('photo',async (error,photo)=>{
-        if(error) console.error(error)
-        if(photo!=null){
-            return res.json(JSON.parse(photo));
-        }
-        else{
-            const {data}=await axios.get(
-                "https://fakestoreapi.com/products",
-            )
-            console.log(data)
-            redisClient.setex("photo",DEFAULT_EXPIRATION,JSON.stringify(data))
-        }
-        res.json(data)
-    })
+    console.log("there")
+    // const {data} =await axios.get(
+    //     "https://fakestoreapi.com/products"
+    // )
+    // redisClient.SETEX("photo",DEFAULT_EXPIRATION,JSON.stringify(data))
+    // redisClient.get('photo',async (error,photo)=>{
+    //     console.log("inside redisclient")
+    //     if(error) console.error(error)
+    //     if(photo!=null){
+    //         return res.json(JSON.parse(photo));
+    //     }
+    //     else{
+    //         const {data}=await axios.get(
+    //             "https://fakestoreapi.com/products",
+    //         )
+    //         console.log(data)
+    //         redisClient.SETEX("photo",DEFAULT_EXPIRATION,JSON.stringify(data))
+    //     }
+    //     console.log("reach");
+    //     res.json(data)
+    // })
+    const data=await redisClient.get('photo');
+    if(data!=null){
+        console.log("data there")
+        return res.json(JSON.parse(data));
+    }
+    else{
+        console.log("data not there")
+        const {data}=await axios.get(
+                    "https://fakestoreapi.com/products",
+                )
+                console.log(data)
+                redisClient.SETEX("photo",DEFAULT_EXPIRATION,JSON.stringify(data))
+        
+        res.json(data);
+    }
 
-        // const {data}=await axios.get(
-        //     "https://fakestoreapi.com/products",
-        // )
-        // console.log(data)
-        // res.json(data)
-
-    
 })
 
-app.listen(3000)
+app.listen(3002)
 
